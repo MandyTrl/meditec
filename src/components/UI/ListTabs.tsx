b24b41a238424235c0eed408836582cfc603a75d
MandyTrl <thorelmandy@gmail.com>
2024-09-09 13:15:44 +0200

    conditional title for tab

--- src/components/UI/ListTabs.tsx
+++ src/components/UI/ListTabs.tsx
@@ -1,12 +1,23 @@
 import { tabs } from "@/utils/Datas/tabs"
 import { Tab } from "@components/UI/Tab"
+import { HospitalContext } from "@/utils/Context"
+import { useContext } from "react"
 
 export const ListTabs = () => {
+	const hospitalCtxt = useContext(HospitalContext)
+	const { hospital } = hospitalCtxt
+
 	return (
 		<ul className="w-full md:w-fit flex">
 			{tabs.map((title: string) => {
+				const titleToRender = !Array.isArray(hospital) ? hospital.name : title
+
 				return (
-					<Tab key={title} title={title} onClick={() => console.log("test")} />
+					<Tab
+						key={titleToRender}
+						title={titleToRender}
+						onClick={() => console.log("test")}
+					/>
 				)
 			})}
 		</ul>
