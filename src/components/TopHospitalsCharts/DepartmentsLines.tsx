0529b1d84a3f72d50925d03b9ba20c825b654ba5
MandyTrl <thorelmandy@gmail.com>
2024-09-10 03:54:51 +0200

    add label for chartlines + responsive css

--- src/components/TopHospitalsCharts/DepartmentsLines.tsx
+++ src/components/TopHospitalsCharts/DepartmentsLines.tsx
@@ -23,47 +23,50 @@ export const DepartmentsLines = () => {
 				Patients by departments
 			</h4>
 
-			<div className="flex flex-wrap">
+			<div className="flex flex-wrap justify-around">
 				{hospital.map((el: Hospital) => {
 					return (
-						<LineChart
-							key={el.name}
-							width={isMobile ? 320 : 525}
-							height={isMobile ? 350 : 250}
-							data={el.hospitalDepartments}
-							margin={{
-								top: isMobile ? 35 : 12,
-								right: isMobile ? 30 : 18,
-								left: isMobile ? 0 : 12,
-								bottom: isMobile ? 35 : 12,
-							}}>
-							<CartesianGrid strokeDasharray="3 3" />
+						<div key={el.name}>
+							<p className="mb-0 md:mb-1">{el.name}</p>
 
-							<XAxis dataKey="department" padding={{ left: 20, right: 20 }} />
-							{!isMobile && <YAxis />}
+							<LineChart
+								width={isMobile ? 320 : 565}
+								height={isMobile ? 350 : 300}
+								data={el.hospitalDepartments}
+								margin={{
+									top: isMobile ? 15 : 12,
+									right: isMobile ? 20 : 12,
+									left: isMobile ? 20 : 12,
+									bottom: isMobile ? 35 : 12,
+								}}>
+								<CartesianGrid strokeDasharray="3 3" />
 
-							<Tooltip />
-							<Legend
-								width={300}
-								wrapperStyle={{
-									bottom: isMobile ? 40 : 20,
-									right: isMobile ? 0 : 90,
-									lineHeight: "50px",
-								}}
-							/>
+								<XAxis dataKey="department" padding={{ left: 20, right: 20 }} />
+								{!isMobile && <YAxis />}
 
-							<Line
-								type="monotone"
-								dataKey="patientsPerDay"
-								stroke="#64BEFF"
-								activeDot={{ r: 4 }}
-							/>
-							<Line
-								type="monotone"
-								dataKey="averageWaitTime"
-								stroke="#EF62FF"
-							/>
-						</LineChart>
+								<Tooltip />
+								<Legend
+									width={300}
+									wrapperStyle={{
+										bottom: isMobile ? 40 : 20,
+										right: isMobile ? 0 : 90,
+										lineHeight: "50px",
+									}}
+								/>
+
+								<Line
+									type="monotone"
+									dataKey="patientsPerDay"
+									stroke="#64BEFF"
+									activeDot={{ r: 4 }}
+								/>
+								<Line
+									type="monotone"
+									dataKey="averageWaitTime"
+									stroke="#EF62FF"
+								/>
+							</LineChart>
+						</div>
 					)
 				})}
 			</div>
