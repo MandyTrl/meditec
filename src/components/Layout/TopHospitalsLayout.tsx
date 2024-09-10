9b77749cb84f100f5d5f3b0c052db904b5f07e93
MandyTrl <thorelmandy@gmail.com>
2024-09-10 07:52:04 +0200

    comment ActiveShape for build

--- src/components/Layout/TopHospitalsLayout.tsx
+++ src/components/Layout/TopHospitalsLayout.tsx
@@ -2,14 +2,12 @@ import React from "react"
 import { Intro } from "@/components/TopHospitalsCharts/Intro"
 import { Hospitalizations } from "@components/TopHospitalsCharts/Hospitalizations"
 import { DepartmentsLines } from "@components/TopHospitalsCharts/DepartmentsLines"
-// import { DepartmentsPie } from "@components/TopHospitalsCharts/DepartmentsPie"
 
 export const TopHospitalsLayout = () => {
 	return (
 		<section className="w-full flex flex-col md:flex-row flex-wrap gap-2">
 			<Intro />
 			<Hospitalizations />
-			{/* <DepartmentsPie /> */}
 
 			<DepartmentsLines />
 		</section>
