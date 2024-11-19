f996171dbefd21d438f5ebbc32a68e01850616ca
MandyTrl <thorelmandy@gmail.com>
2024-11-19 10:50:20 +0100

    cosmetics on ChartHeader

--- src/components/ChartUI/ChartHeader.tsx
+++ src/components/ChartUI/ChartHeader.tsx
@@ -9,17 +9,13 @@ type ChartHeaderProps = {
 
 export const ChartHeader = ({ title, icon, description }: ChartHeaderProps) => {
 	return (
-		<div className="w-full">
-			<div className="flex flex-col">
-				<div className="w-fit flex items-center flex-1">
-					<Image alt="" src={icon} className="w-4 h-4 fill-primary mr-[5px]" />
-					<h3 className="font-semibold">{title}</h3>
-				</div>
-
-				<p className="text-sm italic">{description}</p>
+		<div className="w-full mb-6 flex flex-col">
+			<div className="w-fit flex items-center flex-1">
+				<Image alt="" src={icon} className="w-4 h-4 fill-primary mr-[5px]" />
+				<h3 className="font-semibold text-lg">{title}</h3>
 			</div>
 
-			<div className="w-full h-[1px] bg-primary/20 mt-4 mb-6"></div>
+			<p className="text-sm italic">{description}</p>
 		</div>
 	)
 }
